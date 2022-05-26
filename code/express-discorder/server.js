3c463cdb82a770c12551347bc52e28bf42a7ea7d
Zurik <zurik.phillips@verizon.com>
2022-05-26 12:57:18 -0500

    Added Space

--- code/express-discorder/server.js
+++ code/express-discorder/server.js
@@ -5,6 +5,7 @@ const axios = require("axios").default;
 const app = express();
 const port = 3000;
 
+
 app.use(express.json());
 
 app.get("/", (req, res) => res.send(`
@@ -12,14 +13,14 @@ app.get("/", (req, res) => res.send(`
     <head><title>Success!</title></head>
     <body>
       <h1>Success!</h1>
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.deviantart.com%2Fcyruscloud%2Fart%2FMegaMan-X-Revived-393813312&psig=AOvVaw3x5Oogql2w2Uoxxtlr5585&ust=1653665123905000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLjbree8_fcCFQAAAAAdAAAAABAT" alt="MegaMan ThumbsUp" />
+      <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d747dafd-cb0c-422a-9204-84ef337feec9/d6igsao-91f9caf5-b14a-4906-9812-05db31bec70d.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q3NDdkYWZkLWNiMGMtNDIyYS05MjA0LTg0ZWYzMzdmZWVjOVwvZDZpZ3Nhby05MWY5Y2FmNS1iMTRhLTQ5MDYtOTgxMi0wNWRiMzFiZWM3MGQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Z3pArIKFp1Hw2JnZyV0BhY_FiMONgdSin9GCFHPKits" alt="MegaMan ThumbsUp" />
     </body>
   </html>
 `));
 
 app.post("/github", (req, res) => {
   const content = ":wave: Success!";
-  const avatarUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.deviantart.com%2Fcyruscloud%2Fart%2FMegaMan-X-Revived-393813312&psig=AOvVaw3x5Oogql2w2Uoxxtlr5585&ust=1653665123905000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLjbree8_fcCFQAAAAAdAAAAABAT";
+  const avatarUrl = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d747dafd-cb0c-422a-9204-84ef337feec9/d6igsao-91f9caf5-b14a-4906-9812-05db31bec70d.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q3NDdkYWZkLWNiMGMtNDIyYS05MjA0LTg0ZWYzMzdmZWVjOVwvZDZpZ3Nhby05MWY5Y2FmNS1iMTRhLTQ5MDYtOTgxMi0wNWRiMzFiZWM3MGQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Z3pArIKFp1Hw2JnZyV0BhY_FiMONgdSin9GCFHPKits";
   axios
     .post(process.env.DISCORD_WEBHOOK_URL, {
       content: content,
